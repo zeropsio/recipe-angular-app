45d34acc962024a6b203d03b850c00143186d9b2
Ales Rechtorik <foxxcz@gmail.com>
2022-03-30 08:57:28 +0200

    project diagram v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Todo, Recipe } from '@zerops/zestrat-models';
 import { TodosService } from './todos.service';
@@ -7,12 +7,22 @@ import { TodosService } from './todos.service';
   selector: 'zr-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   recipe: Recipe = JSON.parse(environment.recipeConfig);
   todos: Todo[];
 
-  constructor(private _todosService: TodosService) {
+  @ViewChild('diagramRef')
+  diagramRef: ElementRef<any>;
+
+  constructor(private _todosService: TodosService) { }
+
+  ngAfterViewInit() {
+    this.diagramRef.nativeElement.runRequest({
+      requestUrl: 'https://app-20109.app.zerops.io',
+      requestType: 'GET'
+    });
+
     this.loadTodos();
   }
 
@@ -29,12 +39,26 @@ export class AppComponent {
   }
 
   updateTodo(id: number | string, data: Partial<Todo>) {
+
+    this.diagramRef.nativeElement.runRequest({
+      requestUrl: 'https://api-2014b-3000.app.zerops.io',
+      requestType: 'POST',
+      affectedServices: ['db']
+    });
+
     this._todosService
       .update$(id, data)
       .subscribe(() => this.loadTodos());
   }
 
   loadTodos() {
+
+    this.diagramRef.nativeElement.runRequest({
+      requestUrl: 'https://api-2014b-3000.app.zerops.io',
+      requestType: 'GET',
+      affectedServices: ['db']
+    });
+
     this._todosService
       .findAll$()
       .subscribe((todos) => this.todos = todos)
